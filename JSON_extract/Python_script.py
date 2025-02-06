a1585c7d3e921ee930c516a607fccb26dd40e3f1
kukretinamanTTN <naman.kukreti@tothenew.com>
2025-02-06 23:01:12 +0530

    Assignment-4

---
+++ JSON_extract/Python_script.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import pandas
+import numpy
+
+def fun1 ():
+    print("This is fun1")
+def fun2 ():
+    print("This is fun2")
+
+a = 9
+b = 10
+c = 11
+
+class A :
+    def classfunA ():
+        print("This is class A fun")
+class B :
+    def classfunB ():
+        print("This is class B fun")
