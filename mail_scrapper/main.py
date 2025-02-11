cd61926f1c305c8f60be27dcee30eaf6da5d453b
kukretinamanTTN <naman.kukreti@tothenew.com>
2025-02-12 00:44:45 +0530

    Assignment-6

---
+++ mail_scrapper/main.py
@@ -0,0 +1,14 @@
+from scrapper import parse_vals
+import json
+import maskpass
+
+if __name__ == "__main__":
+    # user = input("Enter Email ID: ")
+    user = "naman.kukreti@tothenew.com"
+    # pw = maskpass.askpass(mask="")
+    pw = 'rqimamupmyijhxdn'
+
+    all_info = {}
+    parse_vals(user, pw, all_info)    
+    all_info = json.dumps(all_info, indent=4)
+    print(all_info)
