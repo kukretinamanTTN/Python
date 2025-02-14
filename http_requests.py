7e1b88b6ae78658d3f8c6c34bd0c72d64e2035bc
kukretinamanTTN <naman.kukreti@tothenew.com>
2025-02-14 12:05:45 +0530

    Request Handling

---
+++ http_requests.py
@@ -0,0 +1,32 @@
+import requests
+import time
+
+#Custom Exception Class
+class ResponseException(Exception):
+    def __init__(self, status, reason):
+        self.status = status
+        self.reason = reason
+    
+    def __str__(self):
+        return f"Status: {self.status}, Reason: {self.reason}"
+
+
+#class to handle request
+class getCall:
+    def __init__(self, url):
+        self.url = url
+    
+    def request(self):
+        time.sleep(0.01)
+        response = requests.get(self.url)
+        if not response.ok: 
+            raise ResponseException(response.status_code, response.reason)
+        return response
+
+
+if __name__ == "__main__":
+    try:
+        get_obj = getCall("https://github.com/kukretinamanTTN/Python")
+        print(get_obj.request())
+    except ResponseException as e:
+        print("Error in request status!", e)
