1024b9fec36725d6f74b363c6e2a5d1b0c79ff13
Naman Kukreti <naman.kukreti@tothenew.com>
2025-03-27 14:32:19 +0530

    Django Assignments 12-16

--- WebD_Django/temp_project/temp_project/settings/base.py
+++ WebD_Django/temp_project/temp_project/settings/base.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+print(f"Running in: {os.getenv('DJANGO_SETTINGS_MODULE')}")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    ''
+    'temp_app'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'temp_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["/home/naman/Python/WebD_Django/temp_project/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ WebD_Django/temp_project/temp_project/settings/production.py
@@ -0,0 +1,4 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.2']
